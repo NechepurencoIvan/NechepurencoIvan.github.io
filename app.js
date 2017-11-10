1651b3e6fe394f0bedfcdd8aad780344a6c9e2ff
NechepurencoIvan <ivan.nechepurenco@yandex.ru>
2017-11-11 01:44:23 +0300

    added first part

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 ﻿$(function() {
-    $('#build-table').click(function() {
-        var my_table = $('<table border="1">');
-        for (var i = 0; i < $('#numrows').val(); ++i) {
-            var row = $('<tr>');
-            for (var j = 0; j < $('#numcols').val(); ++j) {
-                row.append($('<td>').text(i + ', ' + j));
-            }
-            my_table.append(row);
-        }
-        $('#my-table').append(my_table);
-    });
+
     var list = $('<ul>');
     $('#root').append(list);
     var point = $('<li>');
     list.append(point);
-    var text = $('<span>').text("Сделать задание #3 по web-программированию");
-    point.append(text)
+    var txt = $('<span>').text("Сделать задание #3 по web-программированию");
+    point.append(txt)
+    var button1 = $('<button>')
+    point.append(button1.text("Удалить"));
+    button1.click(function() {
+	point.detach()
+    })
 });
